03dd56ac79d80358b08de07039cb1f55fd893df1
Teofilo Jr. Daquipil <me@lemonypancakes.me>
2023-12-30 19:25:09 +0800

    Update .gitignore
    Signed-off-by: Teofilo Jr. Daquipil <me@lemonypancakes.me>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "me.lemonypancakes.resourcemanagerhelper"
@@ -8,4 +9,12 @@ version = "1.0.0-SNAPSHOT"
 dependencies {
     implementation(project(":resourcemanagerhelper-api"))
     implementation(project(":resourcemanagerhelper-v1_17_R1"))
+    implementation(project(":resourcemanagerhelper-v1_18_R1"))
+    implementation(project(":resourcemanagerhelper-v1_18_R2"))
+    implementation(project(":resourcemanagerhelper-v1_19_R1"))
+    implementation(project(":resourcemanagerhelper-v1_19_R2"))
+    implementation(project(":resourcemanagerhelper-v1_19_R3"))
+    implementation(project(":resourcemanagerhelper-v1_20_R1"))
+    implementation(project(":resourcemanagerhelper-v1_20_R2"))
+    implementation(project(":resourcemanagerhelper-v1_20_R3"))
 }
---
+++ resourcemanagerhelper-v1_18_R1/src/main/java/me/lemonypancakes/resourcemanagerhelper/impl/v1_18_R1/NMSResource.java
@@ -0,0 +1,33 @@
+package me.lemonypancakes.resourcemanagerhelper.impl.v1_18_R1;
+
+import me.lemonypancakes.resourcemanagerhelper.Resource;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class NMSResource implements Resource {
+    @Nonnull
+    private final net.minecraft.server.packs.resources.Resource handle;
+
+    public NMSResource(@Nonnull net.minecraft.server.packs.resources.Resource handle) {
+        this.handle = handle;
+    }
+
+    @Nonnull
+    @Override
+    public String sourcePackId() {
+        return this.handle.getSourceName();
+    }
+
+    @Nonnull
+    @Override
+    public InputStream open() throws IOException {
+        return this.handle.getInputStream();
+    }
+
+    @Nonnull
+    public net.minecraft.server.packs.resources.Resource getHandle() {
+        return this.handle;
+    }
+}
