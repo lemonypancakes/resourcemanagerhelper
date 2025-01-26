41d4121f4311769597e160307e2f49cac840da02
Teofilo Jr. Daquipil <contact@lemonypancakes.me>
2025-01-26 18:21:19 +0800

    Fixed NMS package location

--- resourcemanagerhelper-api/src/main/java/me/lemonypancakes/resourcemanagerhelper/ResourceManagerHelper.java
+++ resourcemanagerhelper-api/src/main/java/me/lemonypancakes/resourcemanagerhelper/ResourceManagerHelper.java
@@ -1,23 +1,41 @@
 package me.lemonypancakes.resourcemanagerhelper;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.bukkit.Bukkit;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public final class ResourceManagerHelper {
     @Nullable
     private static ResourceManager resourceManager;
 
     static {
-        String packageName = Bukkit.getServer().getClass().getPackage().getName();
-        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
-
         try {
-            Class<?> clazz = Class.forName("me.lemonypancakes.resourcemanagerhelper.impl." + version + ".NMSResourceManager");
+            String nmsVersion = null;
+            HttpURLConnection conn = (HttpURLConnection) new URL("https://raw.githubusercontent.com/Rapha149/NMSVersions/main/nms-versions.json").openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                JsonObject json = new Gson().fromJson(br.lines().collect(Collectors.joining()), JsonObject.class);
+
+                if (json.has(Bukkit.getBukkitVersion())) {
+                    nmsVersion = json.get(Bukkit.getBukkitVersion()).getAsString();
+                }
+            }
+            Class<?> clazz = Class.forName("me.lemonypancakes.resourcemanagerhelper.impl." + nmsVersion + ".NMSResourceManager");
 
             if (ResourceManager.class.isAssignableFrom(clazz)) {
                 ResourceManagerHelper.resourceManager = (ResourceManager) clazz.getConstructor().newInstance();
