7a6bf0ff939710256b7ad540d6b0457ea0193444
Teofilo Jr. Daquipil <contact@lemonypancakes.me>
2025-01-26 22:13:31 +0800

    Attempt to fix remap

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import io.github.patrick.gradle.remapper.RemapTask
 
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "8.3.5"
     id("maven-publish")
+    id("com.gradleup.shadow") version "8.3.5"
+    id("io.github.patrick.remapper") version "1.4.2"
 }
 
 dependencies {
@@ -22,15 +24,44 @@ dependencies {
     implementation(project(":${rootProject.name}-v1_21_R3"))
 }
 
-tasks {
-    withType<ShadowJar> {
-        archiveClassifier = ""
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.github.patrick.remapper")
+
+    val mcVersion: String by project
+
+    repositories {
+        mavenCentral()
+        maven("https://repo.codemc.io/repository/nms/")
+        maven("https://libraries.minecraft.net/")
+    }
+
+    dependencies {
+        if (project.name != "${rootProject.name}-api") {
+            compileOnly(project(":${rootProject.name}-api"))
+        }
+        compileOnly("org.spigotmc:spigot:$mcVersion-R0.1-SNAPSHOT:remapped-mojang")
+    }
+
+    tasks {
+        remap {
+            dependsOn("shadowJar")
+
+            inputTask.set(getByName<ShadowJar>("shadowJar"))
+            version.set(mcVersion)
+            action.set(RemapTask.Action.MOJANG_TO_SPIGOT)
+        }
+
+        assemble {
+            dependsOn("remap")
+        }
     }
 }
 
 allprojects {
     apply(plugin = "java")
     apply(plugin = "maven-publish")
+    apply(plugin = "com.gradleup.shadow")
 
     val majorVersion = project.property("majorVersion") as String
     val minorVersion = project.property("minorVersion") as String
@@ -52,12 +83,6 @@ allprojects {
         withSourcesJar()
     }
 
-    repositories {
-        mavenCentral()
-        maven("https://repo.codemc.io/repository/nms/")
-        maven("https://libraries.minecraft.net/")
-    }
-
     publishing {
         publications {
             create<MavenPublication>("mavenJava") {
@@ -76,6 +101,18 @@ allprojects {
                     password = System.getenv("JENKINS_PASSWORD")
                 }
             }
+        }
+    }
+
+    tasks {
+        processResources {
+            eachFile {
+                expand("version" to version)
+            }
+        }
+
+        shadowJar {
+            archiveClassifier = ""
         }
     }
 }
